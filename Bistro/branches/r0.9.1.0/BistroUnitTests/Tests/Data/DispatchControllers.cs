4696bf767bff29a237a33ff6f7f98f27af21f43a
kolosy <kolosy@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-07-29 22:12:02 +0000

    unit test cleanup + tweak to resource attributes to allow multiples and aliases.

--- Bistro/branches/r0.9.1.0/BistroUnitTests/Tests/Data/DispatchControllers.cs
+++ Bistro/branches/r0.9.1.0/BistroUnitTests/Tests/Data/DispatchControllers.cs
@@ -12,37 +12,6 @@ using System.Text.RegularExpressions;
 
 namespace Bistro.UnitTests
 {
-    class ControllerValidator : Validator<ValidatingController>
-    {
-        protected override void Define()
-        {
-            this
-                .As("validationTest")
-                .Value(c => c.someField)
-                    .AliasedAs("otherField")
-                    .IsRequired("someField is required")
-                    .IsInRange("a", "zzzzzzzzzzzzzzzzzz", "someField must be alpha")
-                    .IsLongerThan(2, "someField must be at least two characters in length")
-                    .MatchesRegex("ab", RegexOptions.None, "someField must be 'ab'");
-        }
-    }
-
-    [Bind("/validationTest/{someField}")]
-    [ValidateWith(typeof(ControllerValidator))]
-    public class ValidatingController : AbstractController, IValidatable
-    {
-        public string someField;
-
-        [Request]
-        public List<IValidationResult> Messages { get; set; }
-        public bool IsValid { get; set; }
-
-        public override void DoProcessRequest(IExecutionContext context)
-        {
-        }
-    }
-
-
     [Bind("/")]
     public class HomeUrlController1 : AbstractController
     {
---
+++ Bistro/branches/r0.9.1.0/BistroUnitTests/Tests/RuntimeTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NUnit.Framework;
+using Bistro.Controllers;
+using Bistro.Controllers.Dispatch;
+using System.Collections;
+using Bistro.Controllers.OutputHandling;
+using Bistro.Configuration.Logging;
+using Bistro.Configuration;
+using Bistro.Validation;
+using Bistro.UnitTests.Support;
+
+namespace Bistro.UnitTests.Tests
+{
+    [TestFixture]
+    public class RuntimeTests : TestingBase
+    {
+        [Test]
+        public void ApplicationCreated()
+        {
+            Assert.IsNotNull(Application.Instance, "Application not created.");
+        }
+
+        [Test]
+        public void DispatcherCreated()
+        {
+            Assert.IsNotNull(dispatcher);
+        }
+
+    }
+}

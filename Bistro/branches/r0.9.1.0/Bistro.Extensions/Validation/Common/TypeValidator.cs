8df18882e0d1ce8c269200f772a4366fd866f8b6
kolosy <kolosy@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-08-18 05:00:05 +0000

    added a few validation types

--- Bistro/branches/r0.9.1.0/Bistro.Extensions/Validation/Common/TypeValidator.cs
+++ Bistro/branches/r0.9.1.0/Bistro.Extensions/Validation/Common/TypeValidator.cs
@@ -14,7 +14,13 @@ namespace Bistro.Extensions.Validation.Common
         dateISO,
         number,
         digits,
-        url
+        url,
+        alpha,
+        alphanumeric,
+        /// <summary>
+        /// Alphanumeric, with underscores and dashes
+        /// </summary>
+        extendedalphanumeric
     }
     public class TypeValidator<T> : DefaultValidator<T> where T : IValidatable
     {
@@ -42,6 +48,15 @@ namespace Bistro.Extensions.Validation.Common
                     break;
                 case ValidatableTypes.email:
                     re = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$");
+                    break;
+                case ValidatableTypes.alpha:
+                    re = new Regex(@"^[a-zA-Z]*$");
+                    break;
+                case ValidatableTypes.alphanumeric:
+                    re = new Regex(@"^[\w]*$");
+                    break;
+                case ValidatableTypes.extendedalphanumeric:
+                    re = new Regex(@"^[\w-]*$");
                     break;
                 case ValidatableTypes.date:
                     DateTime d;

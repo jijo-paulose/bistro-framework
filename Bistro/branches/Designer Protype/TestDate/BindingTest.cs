6262387fda145ef7cd9ce6518a2ba2821b2b050d
alexander.ordin@gmail.com <alexander.ordin@gmail.com@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-09-03 18:00:22 +0000

    Add TestData project

---
+++ Bistro/branches/Designer Protype/TestDate/BindingTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bistro.Methods;
+
+namespace Bistro.Tests
+{
+    public class BindingTest
+    {
+        public BindingTest(string bindingUrl, params BindingTest[] children)
+        {
+            this.bindingUrl = bindingUrl;
+            foreach (BindingTest child in children)
+                this.children.Add(child.bindingUrl, child);
+            controllers = new ControllerTest[] { };
+        }
+
+        public BindingTest(string bindingUrl, ControllerTest[] controllers, params BindingTest[] children)
+        {
+            this.bindingUrl = bindingUrl;
+            foreach (BindingTest child in children)
+                this.children.Add(child.bindingUrl, child);
+            this.controllers = controllers;
+        }
+        string bindingUrl;
+        Dictionary<string, BindingTest> children = new Dictionary<string,BindingTest>();
+        ControllerTest[] controllers;
+
+        public void Validate(Binding binding)
+        {
+            Validate("", binding);
+        }
+
+        public void Validate(string parentUrl, Binding binding)
+        {
+            string fullUrl = parentUrl + bindingUrl;
+            binding.Bindings.ForEach(child =>
+                {
+                    BindingTest test = null;
+                    string lookup = (child.BindingUrl == child.FullBindingUrl) ? (child.Verb + " " + child.BindingUrl) : child.BindingUrl;
+
+                    test.Validate(fullUrl, child);
+                });
+            
+            SortedList<int, Controller> controllers = new SortedList<int,Controller>();
+            binding.Controllers.ForEach(controller => controllers.Add(controller.SeqNumber, controller));
+
+            for (int i = 0; i < this.controllers.Length && i < binding.Controllers.Count; i++)
+                this.controllers[i].Validate(fullUrl, controllers.Values[binding.Controllers.Count - i - 1]);
+        }
+
+    }
+
+}
---
+++ Bistro/branches/Designer Protype/TestDate/Errors/ErrorDescriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bistro.Tests.Errors
+{
+    public interface IErrorDescriptor
+    {
+        void Validate(IErrorDescriptor errorDesc);
+    }
+
+    public class ErrorDescriptor : IErrorDescriptor
+    {
+        internal ErrorDescriptor()
+        {
+
+        }
+
+        #region IErrorDescriptor Members
+
+        public virtual void Validate(IErrorDescriptor errorDesc)
+        {
+        }
+
+        #endregion
+    }
+}

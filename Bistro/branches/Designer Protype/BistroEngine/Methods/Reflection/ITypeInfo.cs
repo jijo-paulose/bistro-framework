a86b336db9c9aa3112f4766d4087fe25649e6ea2
alexander.ordin@gmail.com <alexander.ordin@gmail.com@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-09-03 17:53:23 +0000

    Add Bistro Engine

---
+++ Bistro/branches/Designer Protype/BistroEngine/Methods/Reflection/ITypeInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bistro.Methods.Reflection
+{
+    public interface IAttributeInfo
+    {
+        string Type { get; }
+        IAttributeParameters Parameters { get; }
+    }
+
+    public interface IAttributeParameters
+    {
+        int Count { get; }
+        IAttributeParameter this[int index] { get; }
+        IAttributeParameter this[string name] { get; }
+    }
+
+    public interface IAttributeParameter
+    {
+        string AsString();
+    }
+
+    public interface IFieldInfo
+    {
+        IEnumerable<IAttributeInfo> Attributes { get; }
+        string Name { get; }
+        string Type { get; }
+    }
+
+    public interface IPropertyInfo
+    {
+        IEnumerable<IAttributeInfo> Attributes { get; }
+        string Name { get; }
+        string Type { get; }
+    }
+
+    public interface ITypeInfo
+    {
+        string FullName { get; }
+        IEnumerable<IAttributeInfo> Attributes { get; }
+        IEnumerable<IFieldInfo> Fields { get; }
+        IEnumerable<IPropertyInfo> Properties { get; }
+    }
+}

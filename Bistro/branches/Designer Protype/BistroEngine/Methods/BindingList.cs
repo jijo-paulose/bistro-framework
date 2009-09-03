4593c77907a4f20130100766e7e9840dd1e48e1c
alexander.ordin <alexander.ordin@gmail.com>
2009-09-03 18:06:05 +0000

    Add BistroMethodEngine project

---
+++ Bistro/branches/Designer Protype/BistroEngine/Methods/BindingList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bistro.Methods
+{
+    internal class BindingList 
+    {
+        SortedDictionary<string, Binding> dictionary = new SortedDictionary<string, Binding>();
+
+        private string buildKey(Binding binding) {return binding.Verb + ';' + binding.BindingUrl;}
+
+        public IEnumerable<Binding> Values { get { return dictionary.Values; } }
+
+        public int Count { get { return dictionary.Count; } }
+
+        public void Add(Binding binding) { dictionary.Add(buildKey(binding), binding); }
+
+        public void Remove(Binding binding) { dictionary.Remove(buildKey(binding)); }
+    }
+}
---
+++ Bistro/branches/Designer Protype/BistroEngine/Methods/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bistro.Methods
+{
+    public class Controller
+    {
+        public Controller(Binding binding, Controller source)
+        {
+            type = source.type;
+            this.binding = binding;
+        }
+
+        public Controller(Binding binding, ControllerType type, string methodUrl)
+        {
+            this.type = type;
+            this.binding = binding;
+        }
+
+        ControllerType type;
+        Binding binding;
+
+        public ControllerType Type { get { return type; } }
+
+        int seqNumber;
+        public int SeqNumber { get { return seqNumber; } set { seqNumber = value; } }
+
+        internal void Dispose()
+        {
+           binding.Unregister(this);
+        }
+    }
+}

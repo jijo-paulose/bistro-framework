6518f13a816da16ce8263ee475a9e9292cc3fd58
ABadylin <ABadylin@hill30.com>
2009-09-10 12:24:56 +0000

    Merge from r0.9.1.0

---
+++ Bistro/trunk/Bistro.Core/Controllers/Dispatch/IControllerDispatcher.cs
@@ -0,0 +1,54 @@
+﻿/****************************************************************************
+ * 
+ *  Bistro Framework Copyright © 2003-2009 Hill30 Inc
+ *
+ *  This file is part of Bistro Framework.
+ *
+ *  Bistro Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bistro Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Bistro Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ ***************************************************************************/
+
+using System;
+using Bistro.Controllers.Descriptor;
+namespace Bistro.Controllers.Dispatch
+{
+    /// <summary>
+    /// Manages controller application to urls. This interface should be implemented only if
+    /// a drastic departure from default dispatch functionality is required
+    /// </summary>
+    public interface IControllerDispatcher
+    {
+        /// <summary>
+        /// Gets a sorted list of controllers that will process the given url
+        /// </summary>
+        /// <param name="requestUrl">The request URL.</param>
+        /// <returns>A sorted list of controllers</returns>
+        ControllerInvocationInfo[] GetControllers(string requestUrl);
+
+        /// <summary>
+        /// Determines whether the specified url has a controller explicitly bound to it
+        /// </summary>
+        /// <param name="requestUrl">The request URL.</param>
+        /// <returns>
+        /// 	<c>true</c> if an exact binding exists; otherwise, <c>false</c>.
+        /// </returns>
+        bool HasExactBind(string requestUrl);
+
+        /// <summary>
+        /// Registers the controller with the dispatcher.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        void RegisterController(ControllerDescriptor info);
+    }
+}
---
+++ Bistro/trunk/Bistro.UnitTests/Support/CustomManager/TestApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bistro.Configuration.Logging;
+
+namespace Bistro.UnitTests.Support.CustomManager
+{
+    public class TestApplication : Application
+    {
+        public TestApplication(ILoggerFactory loggerFactory) : base(loggerFactory) { }
+
+        internal void ResetApp()
+        {
+            Instance = null;
+            Initialized = false;
+        }
+
+    }
+}

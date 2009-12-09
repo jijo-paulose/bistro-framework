5bb4174e17a142af60c796beb1513aee89b3a594
kolosy <kolosy@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-12-09 19:57:16 +0000

    trunk -> branch

--- Bistro/branches/r0.9.1.0/Bistro.Extensions/Properties/AssemblyInfo.cs
+++ Bistro/branches/r0.9.1.0/Bistro.Extensions/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Bistro.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("American Imaging Management")]
+[assembly: AssemblyCompany("Hill 30, Inc")]
 [assembly: AssemblyProduct("Bistro.Extensions")]
-[assembly: AssemblyCopyright("Copyright © American Imaging Management 2009")]
+[assembly: AssemblyCopyright("Copyright © Hill30, Inc 2008,2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: AssemblyVersion("0.9.2.0")]
+[assembly: AssemblyFileVersion("0.9.2.0")]

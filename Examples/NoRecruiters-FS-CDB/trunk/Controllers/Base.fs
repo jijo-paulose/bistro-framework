eab0dfdc8f62aaa306782d1d5810af286ff22bf3
kolosy <kolosy@f2ac5f86-6bf2-11de-afdd-bb9f892af8c1>
2009-12-21 22:44:49 +0000

    initial load

---
+++ Examples/NoRecruiters-FS-CDB/trunk/Controllers/Base.fs
@@ -0,0 +1,16 @@
+﻿#light
+
+namespace NoRecruiters.Controllers
+
+    open Bistro.FS.Controller
+    open Bistro.FS.Definitions
+    open Bistro.FS.Inference
+    
+    open Bistro.Controllers
+    open Bistro.Controllers.Descriptor
+    open Bistro.Controllers.Descriptor.Data
+    
+    module Base =
+        [<Bind("?"); RenderWith("Views/index.django"); ReflectedDefinition>]
+        let helloC (ctx: ictx) = 
+            "hello world" |> named "test"

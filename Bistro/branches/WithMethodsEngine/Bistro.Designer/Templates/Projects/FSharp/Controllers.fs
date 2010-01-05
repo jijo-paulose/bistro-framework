dd4e32566afc2ceabe04644d34cb43b96347f3ea
mfeingold <mfeingold@hill30.com>
2010-01-05 22:14:44 +0000

    Full set of project/item templates

---
+++ Bistro/branches/WithMethodsEngine/Bistro.Designer/Templates/Projects/FSharp/Controllers.fs
@@ -0,0 +1,18 @@
+#light
+
+namespace $nameSpace$
+
+open Bistro.FS.Controller
+open Bistro.FS.Definitions
+open Bistro.FS.Inference
+
+open Bistro.Controllers
+open Bistro.Controllers.Descriptor
+open Bistro.Controllers.Descriptor.Data
+open Bistro.Http
+
+open System.Text.RegularExpressions
+open System.Web
+
+module $className$ = 
+    let Controller1 = ()

540ab729f5af3e1413e26ef67f8f9bffd01fc6e8
mfeingold <mfeingold@hill30.com>
2010-01-22 03:10:03 +0000

    Project System - Page order in the project designer

--- Bistro/branches/WithMethodsEngine/Bistro.Designer/Projects/FSharp/ProjectManager.cs
+++ Bistro/branches/WithMethodsEngine/Bistro.Designer/Projects/FSharp/ProjectManager.cs
@@ -28,11 +28,50 @@ namespace Bistro.Designer.Projects.FSharp
             return VSConstants.S_OK;
         }
 
+        // the fsharp debug project propety page - we need to suppress it
         const string debug_page_guid = "{9CFBEB2A-6824-43e2-BD3B-B112FEBC3772}";
+
+        Dictionary<uint, object> firstChild = new Dictionary<uint, object>();
+        Dictionary<uint, object> nextSibling = new Dictionary<uint, object>();
 
         protected override int GetProperty(uint itemId, int propId, out object property)
         {
+            switch ((__VSHPROPID)propId)
+            {
+                case __VSHPROPID.VSHPROPID_FirstChild:
+                case __VSHPROPID.VSHPROPID_FirstVisibleChild:
+                    if (firstChild.TryGetValue(itemId, out property))
+                        return VSConstants.S_OK;
+                    else
+                        break;
+                case __VSHPROPID.VSHPROPID_NextSibling:
+                case __VSHPROPID.VSHPROPID_NextVisibleSibling:
+                    if (nextSibling.TryGetValue(itemId, out property))
+                        return VSConstants.S_OK;
+                    else
+                        break;
+                default:
+                    break;
+            }
+
             int result = base.GetProperty(itemId, propId, out property);
+            if (result != VSConstants.S_OK)
+                return result;
+
+            switch ((__VSHPROPID)propId)
+            {
+                case __VSHPROPID.VSHPROPID_FirstChild:
+                case __VSHPROPID.VSHPROPID_FirstVisibleChild:
+                    firstChild.Add(itemId, property);
+                    return VSConstants.S_OK;
+                case __VSHPROPID.VSHPROPID_NextSibling:
+                case __VSHPROPID.VSHPROPID_NextVisibleSibling:
+                    nextSibling.Add(itemId, property);
+                    return VSConstants.S_OK;
+                default:
+                    break;
+            }
+
             if (itemId == VSConstants.VSITEMID_ROOT)
             {
                 switch ((__VSHPROPID2)propId)
@@ -43,11 +82,21 @@ namespace Bistro.Designer.Projects.FSharp
                             .Aggregate("", (a, next) => next.Equals(debug_page_guid, StringComparison.OrdinalIgnoreCase) ? a : a + ';' + next).Substring(1);
                         return VSConstants.S_OK;
                     case __VSHPROPID2.VSHPROPID_PropertyPagesCLSIDList:
-                        //Add the CompileOrder property page.
-                        var properties = new List<string>(property.ToString().Split(';'));
-                        properties.Insert(1, typeof(CompileOrderPage).GUID.ToString("B"));
-                        property = properties.Aggregate("", (a, next) => a + ';' + next).Substring(1);
-                        return VSConstants.S_OK;
+                        {
+                            //Add the CompileOrder property page.
+                            var properties = new List<string>(property.ToString().Split(';'));
+                            properties.Add(typeof(CompileOrderPage).GUID.ToString("B"));
+                            property = properties.Aggregate("", (a, next) => a + ';' + next).Substring(1);
+                            return VSConstants.S_OK;
+                        }
+                    case __VSHPROPID2.VSHPROPID_PriorityPropertyPagesCLSIDList:
+                        {
+                            // set the order for the project property pages
+                            var properties = new List<string>(property.ToString().Split(';'));
+                            properties.Insert(1, typeof(CompileOrderPage).GUID.ToString("B"));
+                            property = properties.Aggregate("", (a, next) => a + ';' + next).Substring(1);
+                            return VSConstants.S_OK;
+                        }
                     default:
                         break;
                 }

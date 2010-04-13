9e18c7adb19bd29401a62720afd4b1a2c36a068a
sasha_vlasova <sasha_vlasova@mail.ru>
2010-04-13 12:43:02 +0000

    Write content to xml bug fix

--- Bistro/branches/WithMethodsEngine/ProjectExtender/ProjectExtenderPackage.cs
+++ Bistro/branches/WithMethodsEngine/ProjectExtender/ProjectExtenderPackage.cs
@@ -78,97 +78,43 @@ namespace FSharp.ProjectExtender
             base.Initialize();
             RegisterProjectFactory(new FSharp.ProjectExtender.Factory(this));
 
-            ErrorHandler.ThrowOnFailure(((IVsMonitorSelection2)Package.GetGlobalService(typeof(IVsMonitorSelection))).GetEmptySelectionContext(out selectionTracker));
+            ErrorHandler.ThrowOnFailure(((IVsMonitorSelection2)Package.GetGlobalService(typeof(IVsMonitorSelection))).GetEmptySelectionContext(out this.selectionTracker));
             solution = (IVsSolution)GetService(typeof(SVsSolution));
 
             // Create the command for the project extender
-            CommandID extenderCommandID = new CommandID(Constants.guidProjectExtenderCmdSet, (int)Constants.cmdidProjectExtender);
-            OleMenuCommand projectExtenderCommand = new OleMenuCommand(ProjectExtenderCommand, extenderCommandID);
-            projectExtenderCommand.BeforeQueryStatus += new EventHandler(projectExtenderCommand_BeforeQueryStatus);
+            CommandID toolwndCommandID = new CommandID(Constants.guidProjectExtenderCmdSet, (int)Constants.cmdidProjectExtender);
+            MenuCommand menuToolWin = new MenuCommand(ProjectExtender, toolwndCommandID);
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            mcs.AddCommand(projectExtenderCommand);
+            mcs.AddCommand(menuToolWin);
         }
 
-
-        void projectExtenderCommand_BeforeQueryStatus(object sender, EventArgs e)
-        {
-            ((OleMenuCommand)sender).Enabled = !(get_current_project() is IProjectManager);
-        }
-
-        private static IVsProject get_current_project()
-        {
-            IntPtr ppHier = IntPtr.Zero;
-            uint pitemid;
-            IVsMultiItemSelect ppMIS;
-            IntPtr ppSC;
-            try
-            {
-                ErrorHandler.ThrowOnFailure(selectionTracker.GetCurrentSelection(out ppHier, out pitemid, out ppMIS, out ppSC));
-                return (IVsProject)Marshal.GetObjectForIUnknown(ppHier);
-            }
-            finally
-            {
-                Marshal.Release(ppHier);
-            }
-        }
-
-        static IVsTrackSelectionEx selectionTracker;
+        IVsTrackSelectionEx selectionTracker;
         IVsSolution solution;
 
         #endregion
 
-        private void ProjectExtenderCommand(object sender, EventArgs e)
+        private void ProjectExtender(object sender, EventArgs e)
         {
-            ModifyProject(get_current_project(), enable_extender);
-        }
+            IntPtr ppHier;
+            uint pitemid;
+            IVsMultiItemSelect ppMIS;
+            IntPtr ppSC;
+            ErrorHandler.ThrowOnFailure(selectionTracker.GetCurrentSelection(out ppHier, out pitemid, out ppMIS, out ppSC));
+            var projectRoot = (IVsHierarchy)(IVsProject)Marshal.GetObjectForIUnknown(ppHier);
+            string projFile;
+            ErrorHandler.ThrowOnFailure(projectRoot.GetCanonicalName(VSConstants.VSITEMID_ROOT, out projFile));
+            ErrorHandler.ThrowOnFailure(solution.CloseSolutionElement(0, projectRoot, 0));
 
-        private void ModifyProject(IVsProject vsProject, Action<XmlDocument> effector)
-        {
-            var project = ProjectManager.getFSharpProjectNode(vsProject);
-            var MSBuildProject = project.BuildProject;
-            var minfo = typeof(Microsoft.Build.BuildEngine.Project).GetMethod("get_XmlDocument", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-
-            var projFile = project.GetMkDocument();
-            effector((XmlDocument)minfo.Invoke(MSBuildProject, new object[] { }));
-            project.SetProjectFileDirty(true);
-            
-            ErrorHandler.ThrowOnFailure(solution.CloseSolutionElement(0/*(uint)__VSSLNCLOSEOPTIONS.SLNCLOSEOPT_UnloadProject*/, project, 0));
-
-            var rguidProjectType = Guid.Empty;
-            var iidProject = Guid.Empty;
-            IntPtr ppProject;
-            IVsProjectFactory ppProjectFactory;
-            solution.GetProjectFactory(0, null, projFile, out ppProjectFactory);
-
-            IVsUIShell shell = GetService(typeof(SVsUIShell)) as IVsUIShell;
-            IVsWindowFrame frame = null;
-            Guid persistenceSlot = new Guid(EnvDTE.Constants.vsWindowKindSolutionExplorer);
-            ErrorHandler.ThrowOnFailure(shell.FindToolWindow(0, ref persistenceSlot, out frame));
-            
-            Guid guidProj = typeof(IVsProject).GUID;
-
-            ErrorHandler.ThrowOnFailure(solution.CreateProject(ref rguidProjectType, projFile, null, null,
-                (uint)(__VSCREATEPROJFLAGS.CPF_OPENFILE) + 2048 + 4096,
-                ref iidProject, out ppProject));
-        }
-
-        private const string msBuildNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
-        private static XmlNamespaceManager namespace_manager = NamespaceManager();
-
-        private static XmlNamespaceManager NamespaceManager()
-        {
-            var result = new XmlNamespaceManager(new NameTable());
-            result.AddNamespace("default", msBuildNamespace);
-            return result;
-        }
-
-        private static void enable_extender(XmlDocument project)
-        {
-            var projectTypeGuids = project.SelectSingleNode("//default:Project/default:PropertyGroup/default:ProjectTypeGuids", namespace_manager);
+            var buildFile = new XmlDocument();
+            var projReader = new XmlTextReader(projFile);
+            var nsmgr = new XmlNamespaceManager(projReader.NameTable);
+            nsmgr.AddNamespace("default", "http://schemas.microsoft.com/developer/msbuild/2003");
+            buildFile.Load(projReader);
+            var projectTypeGuids = buildFile.SelectSingleNode("//default:Project/default:PropertyGroup/default:ProjectTypeGuids", nsmgr);
             if (projectTypeGuids == null)
             {
-                projectTypeGuids = project.CreateElement("ProjectTypeGuids", msBuildNamespace);
-                var projectGuid = project.SelectSingleNode("//default:Project/default:PropertyGroup/default:ProjectGuid", namespace_manager);
+                projectTypeGuids = buildFile.CreateElement("ProjectTypeGuids",buildFile.DocumentElement.NamespaceURI);
+                var projectGuid = buildFile.SelectSingleNode("//default:Project/default:PropertyGroup/default:ProjectGuid",nsmgr);
                 projectGuid.ParentNode.InsertAfter(projectTypeGuids, projectGuid);
                 projectTypeGuids.InnerText = "{" + Constants.guidProjectExtenderFactoryString + "};{" + Constants.guidFSharpProject + "}";
             }
@@ -177,7 +123,7 @@ namespace FSharp.ProjectExtender
                 var types = new List<string>(projectTypeGuids.InnerText.Split(';'));
                 if (types[types.Count - 1] != '{' + Constants.guidFSharpProject + '}')
                 {
-                    types.Insert(0, '{' + Constants.guidProjectExtenderFactoryString + '}');
+                    types.Insert(types.Count - 1, '{' + Constants.guidProjectExtenderFactoryString + '}');
 
                     var typestring = "";
                     types.ForEach(type => typestring += ';' + type);
@@ -185,6 +131,16 @@ namespace FSharp.ProjectExtender
                     projectTypeGuids.InnerText = typestring.Substring(1);
                 }
             }
+            projReader.Close();
+            var projWriter = new XmlTextWriter(new StreamWriter(projFile));
+            buildFile.WriteContentTo(projWriter);
+            projWriter.Close();
+            var rguidProjectType = Guid.Empty;
+            var iidProject = Guid.Empty;
+            IntPtr ppProject;
+            ErrorHandler.ThrowOnFailure(solution.CreateProject(ref rguidProjectType, projFile, null, null,
+                (uint)__VSCREATEPROJFLAGS.CPF_OPENFILE,
+                ref iidProject, out ppProject));
         }
     }
 }
